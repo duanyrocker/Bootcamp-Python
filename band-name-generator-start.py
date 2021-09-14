d5c4be70bfca07305c039a8e90d9622861e3a58d
Duany Rocker <duuh31.rocker@gmail.com>
2021-09-14 12:22:53 -0300

    Projetos realizados no bootcamp

---
+++ band-name-generator-start.py
@@ -0,0 +1,11 @@
+# 1. Create a greeting for your program.
+print("Hello, Welcome on band-generator")
+# 2. Ask the user for the city that they grew up in.
+city = input("Whats is name the city that they grew up in?\n")
+# 3. Ask the user for the name of a pet.
+pet = input("Whats is name your pet?\n")
+# 4. Combine the name of their city and pet and show them their band name.
+band_name = city + " " + pet
+print(f"Your band name: {band_name}")
+
+
